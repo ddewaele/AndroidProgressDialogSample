f2057853e87a65f51e49a6670e7edab00038e9f6
Davy De Waele <ddewaele@gmail.com>
2010-11-13 01:05:05 +0100

    added comment

--- src/com/ecs/android/listview/sample/AsyncTaskSimpleConfigured.java
+++ src/com/ecs/android/listview/sample/AsyncTaskSimpleConfigured.java
@@ -1,5 +1,11 @@
 package com.ecs.android.listview.sample;
 
+/**
+ * We're just extending the basic asynctask activity.
+ * 
+ * The manifest for this activity contains : android:configChanges="keyboardHidden|orientation"
+ * 
+ */
 public class AsyncTaskSimpleConfigured extends AsyncTaskSimple {
 
 }
